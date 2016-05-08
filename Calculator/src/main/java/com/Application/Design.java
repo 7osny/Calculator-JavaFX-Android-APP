af592ad86ab5110f5c3916ece671349bb2acfe9d
Mohammed Hosny <mohammedhosny94@hotmail.com>
2016-05-08 02:25:06 +0200

    Create  Class Grid

--- Calculator/src/main/java/com/Application/Design.java
+++ Calculator/src/main/java/com/Application/Design.java
@@ -2,6 +2,7 @@ package com.Application;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 
@@ -66,5 +67,47 @@ public class Design extends VBox{
 	
  	TextField txt= new TextField();
  	
-	
+
+ 	public Design()
+	{
+	Grid gridButtons=new Grid();
+ 	getChildren().addAll(txt,gridButtons);
+ 	setSpacing(7.5);
+	}
+private class Grid extends GridPane {
+	//create All Buttons 
+	public Grid()
+		{
+
+			//add All Buttons to grid 
+			add(b7,0,0);
+			add(b8,1,0);
+			add(b9,2,0);
+			add(div,3,0);
+			add(pow2,4,0);
+			add(c,5,0);
+			add(del,6,0);
+			add(b4,0,1);
+			add(b5,1,1);
+			add(b6,2,1);
+			add(multiple,3,1);
+			add(left_bracket,4,1);
+			add(right_bracket,5,1);
+			add(sin,6,1);
+			add(b1,0,2);
+			add(b2,1,2);
+			add(b3,2,2);
+			add(sub,3,2);
+			add(pow3,4,2);
+			add(sqrt,5,2);
+			add(cos,6,2);
+			add(b0,0,3);
+			add(dot,1,3);
+			add(remainder,2,3);
+			add(pi,3,3);
+			add(add,4,3);
+			add(equal,5,3);
+			add(tan,6,3);
+		}	
+	}
 }
