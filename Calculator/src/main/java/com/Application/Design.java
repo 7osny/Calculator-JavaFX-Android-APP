8084f48ed2e3ec4d7893fc9865da7912ef5f92eb
Mohammed Hosny <mohammedhosny94@hotmail.com>
2016-05-08 02:21:33 +0200

    Create class Design and create Buttons

---
+++ Calculator/src/main/java/com/Application/Design.java
@@ -0,0 +1,70 @@
+package com.Application;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+
+public class Design extends VBox{
+	//Create Calculator Buttons
+
+ 	Button b0 = new Button("0");
+	
+	Button b1 = new Button("1");
+	
+	Button b2 = new Button("2");
+	
+	Button b3 = new Button("3");
+	
+	Button b4 = new Button("4");
+	
+	Button b5 = new Button("5");
+	
+	Button b6 = new Button("6");	
+	
+	Button b7 = new Button("7");		
+	
+	Button b8 = new Button("8");
+	
+	Button b9 = new Button("9");
+	
+	Button div = new Button("/");
+	
+	Button add = new Button("+");
+	
+	Button sub = new Button("-");
+	
+	Button multiple = new Button("*");
+	
+	Button left_bracket = new Button("(");
+	
+	Button right_bracket = new Button(")");	
+	
+	Button dot = new Button(".");
+	
+	Button remainder = new Button("%");
+	
+	Button pi = new Button("π");
+	
+	Button equal = new Button("=");
+	
+	Button pow2 = new Button("X²");
+	
+	Button pow3 = new Button("X³");
+	
+	Button sqrt = new Button("√");
+	
+	Button del = new Button("↵");
+	
+	Button tan=new Button("tan");
+	
+	Button sin=new Button("sin");
+	
+	Button cos=new Button("cos");
+	
+	Button c=new Button("C");
+	
+ 	TextField txt= new TextField();
+ 	
+	
+}
